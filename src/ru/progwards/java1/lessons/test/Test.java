52d7066fb286a289b75af6690eb23948b090a027
Monkey <yumaev.ram@gmail.com>
2020-04-16 16:40:05 +0300

    Test4

---
+++ src/ru/progwards/java1/lessons/test/Test.java
@@ -0,0 +1,7 @@
+package ru.progwards.java1.lessons.test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Сделаю коммит, запущу в репо: робот, проверяй теперь всё это...");
+    }
+}
